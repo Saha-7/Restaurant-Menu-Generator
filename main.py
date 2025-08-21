4844b4c1a3a8e44f3acfdc867dda42c845d8fe0a
saha-7op <saha7pritam@gmail.com>
2025-08-21 20:30:08 +0530

    Langchain helper

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 import streamlit as st
+import langChainHelper
 
 st.title("Restaurant name Generator")
 
 cuisine = st.sidebar.selectbox("Pick a cuisine", ("Indian", "Italian", "Mexican", "American", "Thai","Chinese"))
 
-def generate_restaurant_name_items(cuisine):
-    return{
-        'restaurant_name': 'Curry Delight',
-        'menu_items': 'Samosa, Butter Chicken',
-    }
+
 
 if cuisine:
-    response = generate_restaurant_name_items(cuisine)
+    response = langChainHelper.generate_restaurant_name_items(cuisine)
     st.header(response['restaurant_name'])
     menu_items = response['menu_items'].split(",")
     st.write("**Menu Items**")
